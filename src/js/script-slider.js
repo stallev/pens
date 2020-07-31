b1c80c32e6e6c49ba98cabd87e6ffe81371ed0b4
Alexander <stallev@yahoo.com>
2020-07-31 06:49:20 +0300

    added sleders

---
+++ src/js/script-slider.js
@@ -0,0 +1,18 @@
+console.log(125);
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    speed: 500,
+    easing: 'ease',
+    infinite: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+  });
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,42 @@
-let name = 'User Name';
-let place = 'Flavortown';
+(function(){
+  let left = 0;
+  document.querySelector('.prev').onclick = motionToRight;
+  document.querySelector('.next').onclick = motionToLeft;
+  var cardList = document.querySelector('.card-list');
+  let cardsCount = document.getElementsByClassName('card').length;
+  var firstCard = document.querySelector('.card');
 
-console.log( `Hello ${name}, ready for ${place}?` );
+  //вычисление численного значения ширины элемента
+  function getElementLengthNumber(element) {
+    let elementWidth = getComputedStyle(element).width;
+    let elementNumberLength = elementWidth.indexOf("px");
+    let elementWidthNumber = parseInt(elementWidth.substring(0, elementNumberLength),10);
+    return elementWidthNumber;
+  }
 
-svg4everybody();
+  let cardNumberLength = getElementLengthNumber(firstCard);
+  let allCardsLengths = cardNumberLength*cardsCount;
+  console.log("ширина контейнера равна "+allCardsLengths);
+
+
+  function motionToLeft(){
+    //console.log("motionToLeft");
+    //console.log("left is " + left);
+    if(left > (3*cardNumberLength - allCardsLengths)){
+      left += -cardNumberLength;
+      console.log("left is " + left);
+      cardList.style.marginLeft = left + 'px';
+    }
+  }
+
+  function motionToRight(){
+    //console.log("motionToRight");
+    if (left < 0)
+    {
+      left += cardNumberLength;
+      console.log("left is " + left);
+      cardList.style.marginLeft = left + 'px';
+    }
+  }
+}
+)();
